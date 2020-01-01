ea9648666d040b413b2c4a44b80175edc82f7528
Miwha Geschwind <miwhag@gmail.com>
2020-01-01 15:38:11 +0000

    Done.

---
+++ reverse_each_word.rb
@@ -0,0 +1,9 @@
+def reverse_each_word(sentence1)
+ new_array = [] 
+split_array = sentence1.split(" ")
+split_array.collect do |reversed_words| 
+ new_array << reversed_words.reverse
+end 
+return new_array.join(' ')
+end
+
