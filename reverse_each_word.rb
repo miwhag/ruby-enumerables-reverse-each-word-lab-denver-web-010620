f8c54544a7fbb0798f7788f8ce5e96473e57a811
Miwha Geschwind <miwhag@gmail.com>
2020-01-01 15:36:12 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 def reverse_each_word(sentence1)
-  
+ new_array = [] 
 split_array = sentence1.split(" ")
 split_array.collect do |reversed_words| 
-  puts reversed_words.reverse
+ new_array << reversed_words.reverse
 end 
-return split_array
+return new_array
 end
 
